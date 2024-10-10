488f139644961ae82dca31187a2ffd8fa1056e10
Dulmi Chamathka <chamathkawijesekara23@gmail.com>
2024-10-10 21:43:26 +0530

    fourth commit

--- dashboard-app/src/component/Financial/Financial.jsx
+++ dashboard-app/src/component/Financial/Financial.jsx
@@ -1,8 +1,22 @@
-
+import { iconsImgs } from "../../utils/images"
+import './Financial.css'
 
 const Financial = () => {
   return (
-    <div>Financial</div>
+    <div className="subgrid-two-item grid-common grid-c8">
+      <div className="grid-c-title">
+        <h3 className="grid-c-title-text">Financial Advice</h3>
+        <button className="grid-c-title-icon" type="button">
+          <img src={iconsImgs.plus} />
+        </button>
+      </div>
+      <div className="grid-c8-content">
+        <p className="text text-silver-v1">ldsfsd fasadf sasfasfasfd
+          sfsadasfs sasfaf efawe aserfrafd ....
+        </p>
+      </div>
+      </div>
+
   )
 }
 
